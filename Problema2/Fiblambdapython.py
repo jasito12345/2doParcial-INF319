b754ffbc0d73f8249f0c36784e7082987a8ab0fa
jasito12345 <113585574+jasito12345@users.noreply.github.com>
2022-12-02 14:19:49 -0400

    Update Fiblambdapython.py

--- Problema2/Fiblambdapython.py
+++ Problema2/Fiblambdapython.py
@@ -1,5 +1,5 @@
 from functools import reduce
-def fibo1(count):
+def tribo1(count):
    listA = [0, 1, 1]
 
    any(map(lambda _:listA.append(sum(listA[-3:])),range(3, count)))
@@ -7,12 +7,12 @@ def fibo1(count):
    return listA[:count]
 
   
-fiboRec = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]+x[-3]],
+triboRec = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]+x[-3]],
                                  range(n-3), [0, 1, 1])
 n = input("Cantidad de terminos de la serie \n")
 serie = int(n)
-print("fibonacci Estructurado\n")
-print(fibo1(serie))
-print("fibonacci Recursivo\n")
-print(fiboRec(serie))
+print("Tribonacci de forma estructurada\n")
+print(tribo1(serie))
+print("Tribonacci de forma recursiva\n")
+print(triboRec(serie))
 input()
